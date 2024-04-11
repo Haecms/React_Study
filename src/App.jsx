08aa4844e11f00e15fe2425a4363712f6d9e5b1d
Haecms <whgo12@naver.com>
2024-04-11 17:44:19 +0900

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,53 @@
+import { useState } from "react";
+
+import { CORE_CONCEPTS, EXAMPLES } from "./data.js";
+import Header from "./components/Header/Header.jsx";
+import CoreConcept from "./components/CoreConcept.jsx";
+import TabButton from "./components/TabButton.jsx";
+
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState("");
+
+  function handleSelect(selectedButton) {
+    setSelectedTopic(selectedButton);
+  }
+
+  let tabContent = <p>Please select a topic.</p>;
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
+  }
+
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            {CORE_CONCEPTS.map((conceptItem) => (
+              <CoreConcept key={conceptItem.title} {...conceptItem} />
+            ))}
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton isSelected={selectedTopic === "components"} onSelect={handleSelect} label="components"></TabButton>
+            <TabButton isSelected={selectedTopic === "jsx"} onSelect={handleSelect} label="jsx"></TabButton>
+            <TabButton isSelected={selectedTopic === "props"} onSelect={handleSelect} label="props" ></TabButton>
+            <TabButton isSelected={selectedTopic === "state"} onSelect={handleSelect} label="state" ></TabButton>
+          </menu>
+          {tabContent}
+        </section>
       </main>
     </div>
   );
